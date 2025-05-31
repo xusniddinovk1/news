ce359a41cd06ba082362d54ce8e5bd812c0c95fd
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-05-31 21:24:57 +0500

    setup files

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,11 @@
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-h-_f$b$8y!$0(-a#&vmjg-l0nz^v7!*e(i!bs_k8xk5n*_l6&1'
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 DEBUG = True
 
