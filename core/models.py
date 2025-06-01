3f162a06e5acf9f33817c57a0d7167afe44ab80c
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-06-01 19:12:29 +0500

    update models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class PublishManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishManager, self).get_queryset().filter(status='published')
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    )
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=200, null=False, blank=False)
+    slug = models.SlugField(max_length=200, unique_for_date='publish')
+    context = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10,
+                              choices=STATUS_CHOICES,
+                              default='draft')
+
+    class Meta:
+        ordering = ('-publish',)
+
+    objects = models.Manager()
+    published = PublishManager()
+
+    def __str__(self):
+        return self.title
